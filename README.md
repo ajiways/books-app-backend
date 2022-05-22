99985b1d19b9cd877601fcce05205e3f0e51baeb
ajiways <ajiwayswc@gmail.com>
2022-05-23 06:17:17 +0700

    docker stuff and edited readme

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ Authors - full CRUD
 ```
 
 #### I see no reason to edit or delete comment because they are anonymous
+
+### To run app use:
+
+```
+docker-compose up
+```
+
+(do not forget to change port to 5000 in client app if are you using docker, if not, change it to 4000 (client default api port))
+
+### Client btw: https://github.com/ajiways/books-app-client
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -5,10 +5,16 @@ import {
 import { join } from 'path';
 
 export function getOrmConfig(): TypeOrmModuleOptions {
+  let entitiesPath = '/dist/**/*.entity.js';
+
+  if (process.env.DOCKER !== 'NO') {
+    entitiesPath = '/**/*.entity.js';
+  }
+
   return {
     type: 'mongodb',
     url: process.env.MONGO_URL,
-    entities: [join(process.cwd(), '/dist/**/*.entity.js')],
+    entities: [join(process.cwd(), entitiesPath)],
     synchronize: true,
     useNewUrlParser: true,
     logging: true,
